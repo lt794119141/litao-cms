b1150b3314ba2ebae8bc2a7750719eabedabe218
asus <794119141@qq.com>
2019-12-12 09:33:43 +0800

    提交项目

---
+++ cms-litao/src/main/java/com/litao/cms/controller/IndexController.java
@@ -0,0 +1,24 @@
+package com.litao.cms.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.litao.cms.service.UserService;
+
+/**   
+* @Title: UserController.java 
+* @Package com.wenjiaquan.cms.controller.User 
+* @Description: TODO(用一句话描述该文件做什么) 
+* @author A18ccms A18ccms_gmail_com   
+* @date 2019年12月10日 下午6:23:11 
+* @version V1.0   
+*/
+@Controller
+@RequestMapping("/")
+public class IndexController {
+	@RequestMapping("")
+	public String index() {
+		return "index";
+	}
+}
---
+++ cms-litao/src/main/java/com/litao/cms/controller/LoginController.java
@@ -0,0 +1,13 @@
+package com.litao.cms.controller;
+
+/**   
+* @Title: LoginController.java 
+* @Package com.wenjiaquan.cms.controller 
+* @Description: TODO(用一句话描述该文件做什么) 
+* @author A18ccms A18ccms_gmail_com   
+* @date 2019年12月10日 下午6:24:11 
+* @version V1.0   
+*/
+public class LoginController {
+	
+}
